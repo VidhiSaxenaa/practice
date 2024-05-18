cd2c61aeb403c4111916e9836cc78940f76591db
VidhiSaxenaa <vidhiisaxena076@gmail.com>
2024-05-19 00:30:01 +0530

    Add files via upload

---
+++ src.js
@@ -0,0 +1,33 @@
+function showTemp(response) {
+  let temperature = document.querySelector(".current-temperature-value");
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input").value;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInputElement;
+  let api = "057314561f8344abb8d5d80t6761o6ae";
+  let city = searchInputElement;
+  let apiKey = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${api}&units=metric`;
+  axios.get(apiKey).then(showTemp);
+}
+
+let forms = document.querySelector("#Search-form");
+forms.addEventListener("submit", search);
+
+let time = document.querySelector("#current-time");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[new Date().getDay()];
+let hour = new Date().getHours();
+let minute = new Date().getMinutes();
+time.innerHTML = `${day} ${hour}:${minute}`;
